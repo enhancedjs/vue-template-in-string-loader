3bd0faf1ee9ff6d807b00555c4f46c3a3f8ac9e1
Paleo <paleo@paleo.casa>
2019-11-14 09:49:36 +0100

    Disable prettier

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import loaderUtils from "loader-utils"
 import { basename } from "path"
 import { updateSource } from "./update-source"
 
-export = function(source: string) {
+export = function (source: string) {
   const options = Object.assign(loaderUtils.getOptions(this) || {}, {
     sourceMap: this.sourceMap,
     filePath: this.resourcePath,
